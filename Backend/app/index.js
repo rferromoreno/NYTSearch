2791b69722fa117bbb1ab580a95b010c985742ae
Alan Scheffer <ascheffer@HEXACTA.COM>
2017-03-02 11:28:42 -0300

    Added new functionality to the NYT API Service.

--- Backend/app/index.js
+++ Backend/app/index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const ser = require('./services/nyt.api.service');
+
+
+ser.getNews("20160101", "20170101", "Argentina")
+    .then((data) => {
+        console.log(data);
+    }, (error)=>{
+        console.log(error);
+    });
--- Backend/app/services/nyt.api.service.js
+++ Backend/app/services/nyt.api.service.js
@@ -2,15 +2,32 @@ let request = require('request-promise');
 
 exports.getNews = getNews;
 
-function getNews(beginDate, endDate) {
-    let options = optionTpm;
-    options.qs.begin_date = `${beginDate}`;
-    options.qs.end_date = `${endDate}`;
+//Gets the news.
+//Returns a promise.
+//Optional arguments: 
+//  strings - q, sort, fl (in that order)
+function getNews(beginDate, endDate, ...args) {
+    let options = setOptions(beginDate, endDate, args);
     return request(options);
 }
 
+//Creates the options object.
+function setOptions(beginDate, endDate, ...args) {
+    let options = optionTmp;
+
+    //Overriding options themplate.
+    args.forEach(function(value, index) {
+        let qsOption = qsOptionsEnum[index]; 
+        options.qs[`${qsOption}`] = value[0];
+    });
+    options.qs.begin_date = `${beginDate}`;
+    options.qs.end_date = `${endDate}`;
+    
+    return options;
+}
+
 //Options template for request
-const optionTpm = {
+const optionTmp = {
     method: 'GET',
     uri: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
     qs: {
@@ -20,4 +37,11 @@ const optionTpm = {
         'fl': "web_url,snippet,headline,pub_date"
     },
     json: true
-}
+};
+
+//Query options enum for API call
+const qsOptionsEnum = [
+    'q',
+    'sort',
+    'fl'
+];
