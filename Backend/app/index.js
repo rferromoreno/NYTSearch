aeaa77fc5e4ad25d211e089c0f300de4effece27
Ricardo <rferromoreno@gmail.com>
2017-03-02 11:58:00 -0300

    new routes
    Using Express Router instead of default routing.

--- Backend/app/index.js
+++ Backend/app/index.js
@@ -3,16 +3,24 @@ const ser = require('./services/nyt.api.service');
 
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
+// get an instance of router
+var router = express.Router();
+
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+    // log each request to the console
+    console.log(req.method, req.url);
+    // continue doing what we were doing and go to the route
+    next(); 
 });
 
-app.get('/search/:queryString/:startDate/:endDate', function(req, res) {
-  // Llamada al service
-  // Habria que verificar si estan bien los parametros
-  // Quizas la logica de la validacion hay que ubicarla en otro lado
-  ser.getNews(req.params.startDate, req.params.endDate, req.params.queryString)
+// home page route 
+router.get('/', function(req, res) {
+    res.send('hello world');
+});
+
+router.get('/search/:queryString/:startDate/:endDate', function(req, res) {
+    ser.getNews(req.params.startDate, req.params.endDate, req.params.queryString)
     .then((data) => {
         console.log(data);
         res.send(data);
@@ -21,13 +29,15 @@ app.get('/search/:queryString/:startDate/:endDate', function(req, res) {
         res.send('Error');
     });
 
-  //res.send();
 });
 
-app.get('*', function(req, res) {
-  res.send('404. Not found.');
+router.get('*', function(req, res) {
+    res.send('404');
 });
 
+
+// apply the routes to our application
+app.use('/', router);
 
 app.listen(3000,function(){
   console.log("Live at Port 3000");
