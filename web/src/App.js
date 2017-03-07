c8887e1e9feb4ab0f08ba870f4990a1dbc994d75
Alan Scheffer <ascheffer@HEXACTA.COM>
2017-03-07 12:19:35 -0300

    Refactored App.js in Frontend.

--- web/src/App.js
+++ web/src/App.js
@@ -3,51 +3,47 @@ import logo from "./logo.svg";
 import Botonera from './Botonera';
 import Displayer from './Displayer';
 import "./App.css";
-var dateFormat = require('dateformat');
+import dateFormat from 'dateformat';
 
-class App extends Component {
-
-constructor(props)
-{
+export default 
+  class App extends Component {
+    constructor(props) {
         super(props);
-        this.state = { query: "",
-                       fechaIn:"",
-                       fechaFin:"",
-                       listadoNoticias: [] } ;
+        this.state = { 
+            query: "",
+            fechaIn:"",
+            fechaFin:"",
+            listadoNoticias: [] 
+        };
         this.handleSubmit = this.handleSubmit.bind(this);
-}
+    }
 
-  //state = { listadoNoticias: null };
+    async handleSubmit(event) {
+        event.preventDefault();
+        let termino = event.target.qname.value;
+        let fini = dateFormat(event.target.finicio.value, 'yyyymmdd');
+        let ffin = dateFormat(event.target.ffinal.value, 'yyyymmdd');
+        
+        // solicitamos la informacion a la apiº (nuestro)      
+        let urlGet = `api/search/${termino}/${fini}/${ffin}`;
+        const response = await fetch(urlGet);
+        const listadoNoticias = await response.json();
 
-  handleSubmit = async e => {
-    e.preventDefault();
-    let termino = e.target.qname.value;
-    let fini = dateFormat(e.target.finicio.value, 'yyyymmdd');
-    let ffin = dateFormat(e.target.ffinal.value, 'yyyymmdd');
-    
-    // solicitamos la informacion al service (nuestro)      
-    let urlGet = `api/search/${termino}/${fini}/${ffin}`;
+        this.setState({
+            listadoNoticias: listadoNoticias
+        });
+    }
 
-    const response = await fetch(urlGet);
-    const listadoNoticias = await response.json();
-    this.setState({
-      listadoNoticias: listadoNoticias
-    });
-  };
-
-render() {
-    return (
-          <div className="App">
-            <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />         
-              <h2>New York Times searcher</h2>
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />         
+                    <h2>New York Times searcher</h2>
+                </div>
+                <Botonera onSubmit={ this.handleSubmit }/>
+                <Displayer listadoNoticias={ this.state.listadoNoticias }/>
             </div>
-            <Botonera onSubmit={this.handleSubmit}/>
-            <Displayer listadoNoticias={this.state.listadoNoticias}/>
-          </div>
-          );
-  }
+        );
+    }
 }
-
-export default App;
-
