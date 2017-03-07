430728b659bfc00073d5fb92419976bbd8bd0f31
Ricardo <rferromoreno@gmail.com>
2017-03-07 15:31:47 -0300

    Fixed datepicker

--- web/src/Botonera.js
+++ web/src/Botonera.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+
+require('react-datepicker/dist/react-datepicker.css');
 
 /*Esta clase se ocupa de manejar el componente ¨Botonera ¨ que será el encargado de manejar los inputs
 para las fechas y la query */
 
 class Botonera extends Component{
 
-  constructor(props) {
+  constructor(props) 
+  {
     super(props);
+    this.state = { finicio: null,
+                   ffinal: null } ;
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInicio = this.handleInicio.bind(this);
+    this.handleFinal = this.handleFinal.bind(this);
   }
 
 
-  handleSubmit(event) {
-
+  handleSubmit(event) 
+  {
     this.props.onSubmit(event);
-   
+  }
+
+  handleInicio(date) {
+    this.setState({
+      finicio: date
+    });
+  }
+
+    handleFinal(date) {
+    this.setState({
+      ffinal: date
+    });
   }
 
 
@@ -32,14 +51,12 @@ render()
 
                     <div className="inicio">
                       <legend> Fecha de inicio: </legend>
-                  
-                      <input type="date" name="finicio" required/>
+                  <DatePicker name="finicio" selected={this.state.finicio} onChange={this.handleInicio} required />
                     </div>
 
                     <div className="inicio">
                       <legend> Fecha final: </legend>
-                
-                      <input type="date" name="ffinal" required/>
+                    <DatePicker name="ffinal" selected={this.state.ffinal} onChange={this.handleFinal} required />
                     </div>
 
                     <div className="boton">
