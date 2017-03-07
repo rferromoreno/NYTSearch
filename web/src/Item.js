0ec4a1bd145f39355075e1303dc35e5734e6ce97
Alan Scheffer <ascheffer@HEXACTA.COM>
2017-03-07 12:56:45 -0300

    Refactored Item.js from Frontend

--- web/src/Item.js
+++ web/src/Item.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
 
+/*
+* Esta clase se ocupa de manejar el componente ¨Item¨ que será el encargado de mostrar una noticia
+* que devuele la consulta a nuestra API
+*/
 
-class Item extends Component{
+export default 
+  class Item extends Component {
+    render() {
+        let noticia = this.props.noticia;
+        const newsIsAvailable = noticia.isAvailable;
 
-render()
+        if(newsIsAvailable) 
         {
-            const isAvailable = this.props.isAvailable;
-             let td = null;
-            if(isAvailable) 
-            {td= <td> <a href={this.props.url}>{this.props.url}</a><spam className="disponible"> Disponible </spam> </td> 
-
-             }
-             else
-                {td= <td>{this.props.url}<spam className="noDisponible"> No disponible </spam> </td> 
-
-                }
-
-          return (
-              
-         <tr>
-                <td>{this.props.title}</td>
-                <td>{this.props.snippet}</td>
-                {td}
-        </tr>
-          );
-
+            return (
+                <tr>
+                <td>{noticia.title}</td>
+                <td>{noticia.snippet}</td>
+                <td> 
+                <a href={ noticia.url }>
+                        { noticia.url }
+                </a> 
+                <spam className="disponible"> Disponible </spam> 
+                </td>
+                </tr>
+            );
         }
-
+        else {
+            return (
+                <tr>
+                <td>{noticia.title}</td>
+                <td>{noticia.snippet}</td>
+                <td>
+                    { noticia.url }<spam className="noDisponible"> No disponible </spam> 
+                </td>
+                </tr>
+            );
+        }
+    }
 }
-export default Item;
