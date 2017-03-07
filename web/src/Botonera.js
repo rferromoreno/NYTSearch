6edaf0f92dacda853de12d79cbb607ce8eb77948
Alan Scheffer <ascheffer@HEXACTA.COM>
2017-03-07 12:26:09 -0300

    Refactored Botonera.js

--- web/src/Botonera.js
+++ web/src/Botonera.js
@@ -1,58 +1,47 @@
 import React, { Component } from 'react';
 
-/*Esta clase se ocupa de manejar el componente ¨Botonera ¨ que será el encargado de manejar los inputs
-para las fechas y la query */
+/*
+* Esta clase se ocupa de manejar el componente ¨Botonera¨ que será el encargado de manejar los inputs
+* para las fechas y la query.
+*/
 
-class Botonera extends Component{
+export default
+  class Botonera extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    handleSubmit(event) {
+        this.props.onSubmit(event);   
+    }
 
+    render() {
+        return (
+            <form onSubmit={ this.handleSubmit }>
+                <fieldset>
+                    <div  className="Botonera">
+                        <div className="busqueda">
+                            <legend> Buscar:  </legend>
+                            <input type="text" name="qname" required/>
+                        </div>
 
-  handleSubmit(event) {
+                        <div className="inicio">
+                            <legend> Fecha de inicio: </legend>                      
+                            <input type="date" name="finicio" required/>
+                        </div>
 
-    this.props.onSubmit(event);
-   
-  }
+                        <div className="inicio">
+                            <legend> Fecha final: </legend>                    
+                            <input type="date" name="ffinal" required/>
+                        </div>
 
-
-render()
-        {
-          return (
-        <form onSubmit={this.handleSubmit}>
-            <fieldset>
-                  <div  className="Botonera">
-                    <div className="busqueda">
-                        <legend> Buscar:  </legend>
-                    
-                        <input type="text" name="qname" required/>
-                    </div>
-
-                    <div className="inicio">
-                      <legend> Fecha de inicio: </legend>
-                  
-                      <input type="date" name="finicio" required/>
-                    </div>
-
-                    <div className="inicio">
-                      <legend> Fecha final: </legend>
-                
-                      <input type="date" name="ffinal" required/>
-                    </div>
-
-                    <div className="boton">
-                     <input type="submit" value="Search" />
-                    </div>
-
-                  </div>
-               </fieldset>
-
-          </form>
+                        <div className="boton">
+                            <input type="submit" value="Search" />
+                        </div>
+                      </div>
+                  </fieldset>
+              </form>
           );
-
-        }
-
+      }
 }
-export default Botonera;
